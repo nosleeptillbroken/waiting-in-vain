158eda577e315460f840f6397c0bff00a339c03d
DESKTOP-RIDGAOB\Robert <robwhyte12@gmail.com>
2018-01-27 15:25:31 -0500

    Added a Base for in game UI Added Cooldown Animations to Tower Build Icons Added Placeholder Materials to represent each player, and each tower

---
+++ ggjg2018/Assets/Scripts/UICooldownControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICooldownControl : MonoBehaviour
+{
+    public Image cooldown1;
+    public Image cooldown2;
+    public float cooldownTime;
+    bool isCooldown1;
+    bool isCooldown2;
+
+    void Update ()
+    {
+		if(Input.GetKeyDown("1"))
+        {
+            isCooldown1 = true;
+        }
+        if(Input.GetKeyDown("2"))
+        {
+            isCooldown2 = true;
+        }
+
+        if(isCooldown1)
+        {
+            cooldown1.fillAmount += cooldownTime * Time.deltaTime;
+            if(cooldown1.fillAmount >= 1)
+            {
+                cooldown1.fillAmount = 0;
+                isCooldown1 = false;
+            }
+        }
+        if(isCooldown2)
+        {
+            cooldown2.fillAmount += cooldownTime * Time.deltaTime;
+            if (cooldown2.fillAmount >= 1)
+            {
+                cooldown2.fillAmount = 0;
+                isCooldown2 = false;
+            }
+        }
+	}
+}
