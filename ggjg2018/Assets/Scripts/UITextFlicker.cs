43a45a0b4923d1affe118893bdb95f9ce7f3de89
jthra037 <its_thrasher@live.ca>
2018-01-27 10:22:48 -0500

    Add scrolling transition from splash to menu in single scene

--- ggjg2018/Assets/Scripts/UITextFlicker.cs
+++ ggjg2018/Assets/Scripts/UITextFlicker.cs
@@ -6,47 +6,61 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Text))]
 public class UITextFlicker : MonoBehaviour {
 
-    public bool flickering = true;
+    public bool Flicker
+    {
+        get { return iSFlickering; }
+        set
+        {
+            if (value && !iSFlickering)
+            {
+                flicker = StartCoroutine(runFlicker());
+                iSFlickering = value;
+            }
+
+            if (iSFlickering && !value)
+            {
+                StopCoroutine(flicker);
+                iSFlickering = value;
+            }
+        }
+    }
+
+
     public float period = 3f;
     [Range(0.01f, 1f)]
     public float stepSize = 0.1f;
     [Range(0f, 1f)]
     public float minimumAlpha = 0.2f;
 
+    private bool iSFlickering = true;
     private Text text;
     private Coroutine flicker;
-    private bool isFlickering = false;
 
 	// Use this for initialization
 	void Start ()
     {
         text = GetComponent<Text>();
+        if (iSFlickering)
+        {
+            flicker = StartCoroutine(runFlicker());
+        }
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if (flickering && !isFlickering)
-        {
-            flicker = StartCoroutine(runFlicker());
-        }
-
-        if (!flickering && isFlickering)
-        {
-            StopCoroutine(flicker);
-            isFlickering = false;
-        }
+        
 	}
 
     private IEnumerator runFlicker()
     {
-        isFlickering = true;
+        iSFlickering = true;
         float x = 0;
 
         float a = 1 - minimumAlpha;
         float c = (a / 2) + minimumAlpha;
 
-        while (flickering)
+        while (iSFlickering)
         {
             float newAlpha = a * Mathf.Cos((period / Mathf.PI) * x) + c;
             Color newColor = text.color;
