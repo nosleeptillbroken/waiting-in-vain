0829cebe2e141f5ea9459c16df6997b4a1aca1ac
Cody <webb.cody@hotmail.com>
2018-01-27 14:57:16 -0500

    Menu navigation for keyboard and mouse works. SFX for button and background music added.

--- ggjg2018/Assets/Scripts/QuitScript.cs
+++ ggjg2018/Assets/Scripts/QuitScript.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuitButton : MonoBehaviour {
+public class QuitScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 }
