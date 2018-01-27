d5d956a6db3af46b74d3ab61c91b54af4f5d949b
MarkMiller <mark-miller04@outlook.com>
2018-01-27 17:53:27 -0500

    - Add hypothetical dictionary functionality while waiting to get my hands on the level generator.

--- ggjg2018/Assets/Scripts/GameManagement/GameManager.cs
+++ ggjg2018/Assets/Scripts/GameManagement/GameManager.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
     //External References.
     SettingsToken settings;
+    //HexGrid board;
 
     //Timer variables.
     private float timerValue;
     private float endgameTime;
     private float totalGameTime;
+
+    //Tile dictionary.
+    //Dictionary<string, GameTile> tileList = new Dictionary<string, HexGrid>();
 
     void Start ()
     {
@@ -23,5 +29,24 @@ public class GameManager : MonoBehaviour
     private void Initialize()
     {
         settings = GameObject.FindGameObjectWithTag("Settings").GetComponent<SettingsToken>();
+        //board = GameObject.FindGameObjectWithTag("Board").GetComponent<HexGrid>();
     }
+
+    /*
+    public GameTile LookupTileData(string key)
+    {
+        GameTile tile = tileList.TryGetValue(key, out  GameTile value);
+        return tile;
+    }
+    
+    public void RegisterTileData(string key, GameTile reference)
+    {
+        tileList.add(key, reference);
+    } 
+
+    */
+
+
+
+
 }
