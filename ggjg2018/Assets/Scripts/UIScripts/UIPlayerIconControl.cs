26b456879053e8261b6eaefb7639511eac9593fd
DESKTOP-RIDGAOB\Robert <robwhyte12@gmail.com>
2018-01-27 18:40:04 -0500

    Added Functional Energy Bar Added Changeable Player Icons to denote land lost or gained Added Placeholder Assets for Player Icons

---
+++ ggjg2018/Assets/Scripts/UIScripts/UIPlayerIconControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayerIconControl : MonoBehaviour
+{
+	
+    public Image playerCard;
+
+    public Sprite[] playerIcons;
+
+    void Start()
+    {
+        playerCard.sprite = playerIcons[0];
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown("p"))
+        {
+            landGained();
+        }
+        if (Input.GetKeyDown("l"))
+        {
+            landLost();
+        }
+    }
+
+    public void landGained()
+    {
+        playerCard.sprite = playerIcons[1];
+
+        StartCoroutine(IconSwapDelay(2));
+    }
+
+    public void landLost()
+    {
+        playerCard.sprite = playerIcons[2];
+
+        StartCoroutine(IconSwapDelay(2));
+    }
+
+    IEnumerator IconSwapDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+        playerCard.sprite = playerIcons[0];
+    }
+}
