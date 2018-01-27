b324b917b35c81d54bdb0f52c6062b99aa4177ec
Dan Yeomans <danyeomans98@gmail.com>
2018-01-26 23:49:00 -0500

    Add hexagonal grid based on catlikecoding tutorials

---
+++ ggjg2018/Assets/Classes/HexMetrics.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class HexMetrics
+{
+
+    public const float outerRadius = 10f;
+
+    public const float innerRadius = 8.66025404f;
+
+    public const float solidFactor = 0.75f;
+
+    public const float blendFactor = 1f - solidFactor;
+
+    public static Vector3[] corners = 
+    {
+        new Vector3(0f, 0f, outerRadius),
+        new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(0f, 0f, -outerRadius),
+        new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(0f, 0f, outerRadius)
+    };
+
+    public static Vector3 GetFirstCorner(HexDirection direction)
+    {
+        return corners[(int)direction];
+    }
+
+    public static Vector3 GetSecondCorner(HexDirection direction)
+    {
+        return corners[(int)direction + 1];
+    }
+
+    public static Vector3 GetFirstSolidCorner(HexDirection direction)
+    {
+        return corners[(int)direction] * solidFactor;
+    }
+
+    public static Vector3 GetSecondSolidCorner(HexDirection direction)
+    {
+        return corners[(int)direction + 1] * solidFactor;
+    }
+
+}
---
+++ ggjg2018/Assets/Editor/CameraControllerEditor.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[@CustomEditor(typeof(CameraController))]
+public class CameraControllerEditor : Editor
+{
+
+    void OnSceneGUI()
+    {
+        CameraController controller = (CameraController)target;
+
+        if (controller != null && controller.cameraBoundsRestriction)
+        {
+
+            Vector3 min = controller.minimumBounds = Handles.PositionHandle(controller.minimumBounds, Quaternion.identity);
+            Vector3 max = controller.maximumBounds = Handles.PositionHandle(controller.maximumBounds, Quaternion.identity);
+
+            Handles.color = Color.red;
+
+            // draw the camera bounds (in red)
+
+            Handles.DrawLine(new Vector3(min.x, min.y, min.z), new Vector3(max.x, min.y, min.z));
+            Handles.DrawLine(new Vector3(min.x, min.y, min.z), new Vector3(min.x, min.y, max.z));
+            Handles.DrawLine(new Vector3(max.x, min.y, min.z), new Vector3(max.x, min.y, max.z));
+            Handles.DrawLine(new Vector3(min.x, min.y, max.z), new Vector3(max.x, min.y, max.z));
+
+            Handles.DrawLine(new Vector3(min.x, min.y, min.z), new Vector3(min.x, max.y, min.z));
+            Handles.DrawLine(new Vector3(max.x, min.y, max.z), new Vector3(max.x, max.y, max.z));
+            Handles.DrawLine(new Vector3(max.x, min.y, min.z), new Vector3(max.x, max.y, min.z));
+            Handles.DrawLine(new Vector3(min.x, min.y, max.z), new Vector3(min.x, max.y, max.z));
+
+            Handles.DrawLine(new Vector3(min.x, max.y, min.z), new Vector3(max.x, max.y, min.z));
+            Handles.DrawLine(new Vector3(min.x, max.y, min.z), new Vector3(min.x, max.y, max.z));
+            Handles.DrawLine(new Vector3(max.x, max.y, min.z), new Vector3(max.x, max.y, max.z));
+            Handles.DrawLine(new Vector3(min.x, max.y, max.z), new Vector3(max.x, max.y, max.z));
+
+            // draw the camera zoom range (in blue)
+
+            Handles.color = Color.blue;
+
+            Vector3 zoomedIn = controller.transform.position;
+            zoomedIn.y = controller.zoomedInHeight;
+            controller.zoomedInHeight = Handles.ScaleValueHandle(controller.zoomedInHeight, zoomedIn, Quaternion.identity, HandleUtility.GetHandleSize(zoomedIn), Handles.SphereCap, 0);
+
+            Vector3 zoomedOut = controller.transform.position;
+            zoomedOut.y = controller.zoomedOutHeight;
+            controller.zoomedOutHeight = Handles.ScaleValueHandle(controller.zoomedOutHeight, zoomedOut, Quaternion.identity, HandleUtility.GetHandleSize(zoomedOut), Handles.SphereCap, 0);
+
+            Handles.DrawLine(zoomedIn, zoomedOut);
+        }
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+        }
+
+    }
+
+}
