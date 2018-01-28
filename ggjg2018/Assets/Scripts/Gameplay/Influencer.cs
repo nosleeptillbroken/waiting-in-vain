5b6b6b116f036936f6f705e8deb33f8a517d7dec
Dan Yeomans <danyeomans98@gmail.com>
2018-01-28 01:00:08 -0500

    Add distance-scaled acquisition of tiles based on influence

---
+++ ggjg2018/Assets/Scripts/Gameplay/Influencer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Influencer : MonoBehaviour {
+
+    public int owner;
+
+    public int influence;
+
+    public bool CanInfluence(GameTile tile)
+    {
+        return true;
+    }
+}
