a19da4835c7ae9a06dfc00aeec8e6db0b5d5ac08
DESKTOP-RIDGAOB\Robert <robwhyte12@gmail.com>
2018-01-27 21:37:17 -0500

    Added several UI layout options configured to buttons

---
+++ ggjg2018/Assets/Scripts/UIScripts/UILayoutOptions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILayoutOptions : MonoBehaviour
+{
+    public GameObject[] layoutOptions;
+    public int currentVisible = 0;
+
+    public void setVisible(int idNum)
+    {
+        layoutOptions[currentVisible].SetActive(false);
+        layoutOptions[idNum].SetActive(true);
+        currentVisible = idNum;
+    } 
+}
