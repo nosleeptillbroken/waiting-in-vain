c5375f9e17335758755c22adb74e26d696a9ef85
Stephen Thompson <Satlazer@hotmail.com>
2018-01-28 13:32:33 -0500

    Added prefab for tile capture, and constructing towers

---
+++ ggjg2018/Assets/Scripts/Simple/TemporaryObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemporaryObject : MonoBehaviour 
+{
+	public float lifetime = 10.0f;
+	float time = 0.0f;
+
+	public TemporaryObject(float _lifetime)
+	{
+		lifetime = _lifetime;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		Destroy(this.gameObject, lifetime);
+	}
+	
+	// Update is called once per frame
+	//void FixedUpdate () 
+	//{
+	//	time += Time.fixedDeltaTime;
+	//	if(time > lifetime)
+	//	{
+	//		DestroyObject(this.gameObject);
+	//	}
+	//}
+}
