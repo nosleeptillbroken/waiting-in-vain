c043c48f6d45ece8275dcc96bc9639a7c03d8811
Andrew MacMillan <andrewwm96@gmail.com>
2018-01-27 19:51:18 -0500

    Added rudimentary controller assignment for up to four players

---
+++ ggjg2018/Assets/Scripts/Player/ControllerAssigner.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+[AddComponentMenu("")]
+public class ControllerAssigner : MonoBehaviour {
+
+    #region static references
+    // Static
+
+    private static ControllerAssigner instance;
+
+    public static Rewired.Player GetRewiredPlayer(int gamePlayerId)
+    {
+        if (!Rewired.ReInput.isReady) return null;
+        if (instance == null)
+        {
+            Debug.LogError("Not initialized. Do you have a PressStartToJoinPlayerSelector in your scehe?");
+            return null;
+        }
+        for (int i = 0; i < instance.playerMap.Count; i++)
+        {
+            if (instance.playerMap[i].gamePlayerId == gamePlayerId) return ReInput.players.GetPlayer(instance.playerMap[i].rewiredPlayerId);
+        }
+        return null;
+    }
+    #endregion
+
+
+    // Instance
+
+    public int maxPlayers = 4;
+
+    private List<PlayerMap> playerMap; // Maps Rewired Player ids to game player ids
+    private int gamePlayerIdCounter = 0;
+
+    void Awake()
+    {
+        playerMap = new List<PlayerMap>();
+        instance = this; // set up the singleton
+    }
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        // Watch for JoinGame action in each Player
+        for (int i = 0; i < ReInput.players.playerCount; i++)
+        {
+            if (ReInput.players.GetPlayer(i).GetButtonDown("JoinGame"))
+            {
+                AssignNextPlayer(i);
+            }
+        }
+
+    }
+
+    void AssignNextPlayer(int rewiredPlayerId)
+    {
+        if (playerMap.Count >= maxPlayers)
+        {
+            Debug.LogError("Max player limit already reached!");
+            return;
+        }
+
+        int gamePlayerId = GetNextGamePlayerId();
+
+        // Add the Rewired Player as the next open game player slot
+        playerMap.Add(new PlayerMap(rewiredPlayerId, gamePlayerId));
+
+        Player rewiredPlayer = ReInput.players.GetPlayer(rewiredPlayerId);
+
+        // Disable the Assignment map category in Player so no more JoinGame Actions return
+        rewiredPlayer.controllers.maps.SetMapsEnabled(false, "Assignment");
+
+        // Enable UI control for this Player now that he has joined
+        rewiredPlayer.controllers.maps.SetMapsEnabled(true, "Default");
+
+        Debug.Log("Added Rewired Player id " + rewiredPlayerId + " to game player " + gamePlayerId);
+    }
+
+    private int GetNextGamePlayerId()
+    {
+        return gamePlayerIdCounter++;
+    }
+
+    // This class is used to map the Rewired Player Id to your game player id
+    private class PlayerMap
+    {
+        public int rewiredPlayerId;
+        public int gamePlayerId;
+
+        public PlayerMap(int rewiredPlayerId, int gamePlayerId)
+        {
+            this.rewiredPlayerId = rewiredPlayerId;
+            this.gamePlayerId = gamePlayerId;
+        }
+    }
+}
---
+++ ggjg2018/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerController : MonoBehaviour {
+
+    public int gamePlayerId = 0;
+
+    private Rewired.Player player { get { return ControllerAssigner.GetRewiredPlayer(gamePlayerId); } }
+
+    //input variables
+    private bool isSelecting;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void GetInputs()
+    {
+        isSelecting = player.GetButtonDown("Select");
+    }
+
+    void ProcessInputs()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    { 
+        if(!ReInput.isReady) return; // Exit if Rewired isn't ready. This would only happen during a script recompile in the editor.
+            if(player == null) return;
+
+        GetInputs();
+        ProcessInputs();
+	}
+}
