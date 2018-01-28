9a0ebd2ef9ffce4c857e139b99786d6e27792772
DESKTOP-RIDGAOB\Robert <robwhyte12@gmail.com>
2018-01-28 12:55:05 -0500

    Added functionality to the pause menu

--- ggjg2018/Assets/Scripts/UIScripts/PauseMenuControl.cs
+++ ggjg2018/Assets/Scripts/UIScripts/PauseMenuControl.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
 
 public class PauseMenuControl : MonoBehaviour
 {
+
+    public GameObject pausePanel;
+    public GameObject[] pauseButtons;
+    public int counter = 0;
 
 	void Start ()
     {
@@ -13,7 +20,35 @@ public class PauseMenuControl : MonoBehaviour
 	
 	void Update ()
     {
-		
-	}
+		if(Input.GetKeyDown("q"))
+        {
+            Time.timeScale = 0.0f;
+            pausePanel.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(pauseButtons[0]);
+        }
+
+        if(Input.GetKeyDown("w"))
+        {
+            counter++;
+            EventSystem.current.SetSelectedGameObject(pauseButtons[counter]);
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            counter--;
+            EventSystem.current.SetSelectedGameObject(pauseButtons[counter]);
+        }
+    }
+
+    public void resumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
 
 }
