a166f52c414709334ce601eecbe15bc956a74631
DESKTOP-RIDGAOB\Robert <robwhyte12@gmail.com>
2018-01-27 19:07:09 -0500

    Added Variable Tooltips based on whether the current tile has a tower or not

---
+++ ggjg2018/Assets/Scripts/UIScripts/UIBuildOptionsControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBuildOptionsControl : MonoBehaviour
+{
+    public Text buildOption1;
+    public Text buildOption2;
+
+    public bool hasTower;
+
+    void Start()
+    {
+        hasTower = true;    
+    }
+
+    void Update()
+    {
+        if(hasTower)
+        {
+            buildOption1.text = "A: Upgrade Range";
+            buildOption2.text = "B: Upgrade Strength";
+        }   
+        else
+        {
+            buildOption1.text = "X: Build Area Tower";
+            buildOption2.text = "Y: Build Directed Tower";
+        } 
+    }
+}
